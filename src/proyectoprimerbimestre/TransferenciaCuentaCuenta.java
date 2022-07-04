5c5cbfa6a881e832bd2e644144ef7c4e2b67f47b
Kevin Martinez <kevinprincipal9@gmail.com>
2022-07-03 23:58:23 -0500

    Cambio en el diseño de la pantalla TransferenciaCuentaCuenta

--- src/proyectoprimerbimestre/TransferenciaCuentaCuenta.java
+++ src/proyectoprimerbimestre/TransferenciaCuentaCuenta.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package proyectoprimerbimestre;
 
-/**
- *
- * @author nivek
- */
 public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
 
-    /**
-     * Creates new form TransferenciaCuentaCuenta
-     */
     Usuario cliente;
     public TransferenciaCuentaCuenta(Usuario cliente) {
         initComponents();
@@ -35,9 +25,7 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
         txtSaldoOrigen = new javax.swing.JLabel();
         txtValorTransferencia = new javax.swing.JLabel();
         txtSeleccioneCuenta = new javax.swing.JLabel();
-        txtNumeroDeCuenta = new javax.swing.JLabel();
         txtCuentaOrigen = new javax.swing.JLabel();
-        txtCantidadTransferencia = new javax.swing.JLabel();
         txtSubtituloDestino = new javax.swing.JLabel();
         txtSubtituloResumen = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
@@ -47,6 +35,12 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
         txtConfirmar = new javax.swing.JLabel();
         btnCancelar = new javax.swing.JPanel();
         txtCancelar = new javax.swing.JLabel();
+        btnValidar = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jComboBox3 = new javax.swing.JComboBox<>();
+        btnValidar1 = new javax.swing.JButton();
+        btnValidar2 = new javax.swing.JButton();
+        txtSaldoOrigen1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -90,8 +84,8 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
 
         txtSaldoOrigen.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
         txtSaldoOrigen.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        txtSaldoOrigen.setText("$1012.25");
-        panelFondoBlanco.add(txtSaldoOrigen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 280, -1));
+        txtSaldoOrigen.setText("$");
+        panelFondoBlanco.add(txtSaldoOrigen, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 240, 20, -1));
 
         txtValorTransferencia.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
         txtValorTransferencia.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -103,31 +97,21 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
         txtSeleccioneCuenta.setText("Seleccione la cuenta destino:");
         panelFondoBlanco.add(txtSeleccioneCuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 300, 280, -1));
 
-        txtNumeroDeCuenta.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
-        txtNumeroDeCuenta.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        txtNumeroDeCuenta.setText("N° 12324568798");
-        panelFondoBlanco.add(txtNumeroDeCuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 280, -1));
-
         txtCuentaOrigen.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
         txtCuentaOrigen.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        txtCuentaOrigen.setText("Cuenta origen:  ");
+        txtCuentaOrigen.setText("Seleccione la cuenta origen:");
         panelFondoBlanco.add(txtCuentaOrigen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 100, 280, -1));
-
-        txtCantidadTransferencia.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
-        txtCantidadTransferencia.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        txtCantidadTransferencia.setText("$156.35");
-        panelFondoBlanco.add(txtCantidadTransferencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 240, 280, -1));
 
         txtSubtituloDestino.setFont(new java.awt.Font("Segoe UI Variable", 1, 12)); // NOI18N
         txtSubtituloDestino.setText("CUENTA DESTINO");
         panelFondoBlanco.add(txtSubtituloDestino, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, -1, -1));
 
         txtSubtituloResumen.setFont(new java.awt.Font("Segoe UI Variable", 1, 12)); // NOI18N
-        txtSubtituloResumen.setText("RESUMEN DE LA TRANSFERENCIA");
+        txtSubtituloResumen.setText("CUENTA ORIGEN");
         panelFondoBlanco.add(txtSubtituloResumen, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, -1, -1));
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        panelFondoBlanco.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 330, 180, -1));
+        panelFondoBlanco.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, 170, -1));
 
         txtSaldo.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
         txtSaldo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -180,6 +164,31 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
 
         panelFondoBlanco.add(btnCancelar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 380, -1, -1));
 
+        btnValidar.setText("Validar");
+        panelFondoBlanco.add(btnValidar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 240, 70, -1));
+
+        jTextField1.setText("10000.00");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        panelFondoBlanco.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 240, 60, -1));
+
+        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        panelFondoBlanco.add(jComboBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 330, 170, -1));
+
+        btnValidar1.setText("Validar");
+        panelFondoBlanco.add(btnValidar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 330, 70, -1));
+
+        btnValidar2.setText("Validar");
+        panelFondoBlanco.add(btnValidar2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 70, -1));
+
+        txtSaldoOrigen1.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtSaldoOrigen1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtSaldoOrigen1.setText("$1012.25");
+        panelFondoBlanco.add(txtSaldoOrigen1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 280, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -193,6 +202,10 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -232,18 +245,22 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel btnCancelar;
     private javax.swing.JPanel btnConfirmar;
+    private javax.swing.JButton btnValidar;
+    private javax.swing.JButton btnValidar1;
+    private javax.swing.JButton btnValidar2;
     private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox3;
     private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JPanel panelAzulFooter;
     private javax.swing.JPanel panelFondoBlanco;
     private javax.swing.JPanel panelRojoFooter;
     private javax.swing.JLabel txtCancelar;
-    private javax.swing.JLabel txtCantidadTransferencia;
     private javax.swing.JLabel txtConfirmar;
     private javax.swing.JLabel txtCuentaOrigen;
-    private javax.swing.JLabel txtNumeroDeCuenta;
     private javax.swing.JLabel txtSaldo;
     private javax.swing.JLabel txtSaldoOrigen;
+    private javax.swing.JLabel txtSaldoOrigen1;
     private javax.swing.JLabel txtSeleccioneCuenta;
     private javax.swing.JLabel txtSubtituloDestino;
     private javax.swing.JLabel txtSubtituloResumen;
