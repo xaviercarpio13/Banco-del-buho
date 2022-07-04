4d29361102fefd599371def79c6a6db77637dafa
Kevin Martinez <kevinprincipal9@gmail.com>
2022-07-03 23:09:57 -0500

    Diseño previo en la pantalla de transferencias cuenta-cuednta

--- src/proyectoprimerbimestre/TransferenciaCuentaCuenta.java
+++ src/proyectoprimerbimestre/TransferenciaCuentaCuenta.java
@@ -26,17 +26,167 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        panelFondoBlanco = new javax.swing.JPanel();
+        txtTituloTransferencia = new javax.swing.JLabel();
+        panelAzulFooter = new javax.swing.JPanel();
+        panelRojoFooter = new javax.swing.JPanel();
+        txtSaldoOrigen = new javax.swing.JLabel();
+        txtValorTransferencia = new javax.swing.JLabel();
+        txtSeleccioneCuenta = new javax.swing.JLabel();
+        txtNumeroDeCuenta = new javax.swing.JLabel();
+        txtCuentaOrigen = new javax.swing.JLabel();
+        txtCantidadTransferencia = new javax.swing.JLabel();
+        txtSubtituloDestino = new javax.swing.JLabel();
+        txtSubtituloResumen = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        txtSaldo = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        btnConfirmar = new javax.swing.JPanel();
+        txtConfirmar = new javax.swing.JLabel();
+        btnCancelar = new javax.swing.JPanel();
+        txtCancelar = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        panelFondoBlanco.setBackground(new java.awt.Color(255, 255, 255));
+        panelFondoBlanco.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        txtTituloTransferencia.setFont(new java.awt.Font("Segoe UI Variable", 1, 18)); // NOI18N
+        txtTituloTransferencia.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtTituloTransferencia.setText("Transferir a otra cuenta");
+        panelFondoBlanco.add(txtTituloTransferencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 20, 280, -1));
+
+        panelAzulFooter.setBackground(new java.awt.Color(15, 34, 64));
+
+        javax.swing.GroupLayout panelAzulFooterLayout = new javax.swing.GroupLayout(panelAzulFooter);
+        panelAzulFooter.setLayout(panelAzulFooterLayout);
+        panelAzulFooterLayout.setHorizontalGroup(
+            panelAzulFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 281, Short.MAX_VALUE)
+        );
+        panelAzulFooterLayout.setVerticalGroup(
+            panelAzulFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 30, Short.MAX_VALUE)
+        );
+
+        panelFondoBlanco.add(panelAzulFooter, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 470, 281, 30));
+
+        panelRojoFooter.setBackground(new java.awt.Color(227, 29, 26));
+
+        javax.swing.GroupLayout panelRojoFooterLayout = new javax.swing.GroupLayout(panelRojoFooter);
+        panelRojoFooter.setLayout(panelRojoFooterLayout);
+        panelRojoFooterLayout.setHorizontalGroup(
+            panelRojoFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 281, Short.MAX_VALUE)
+        );
+        panelRojoFooterLayout.setVerticalGroup(
+            panelRojoFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 10, Short.MAX_VALUE)
+        );
+
+        panelFondoBlanco.add(panelRojoFooter, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 460, 281, 10));
+
+        txtSaldoOrigen.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtSaldoOrigen.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtSaldoOrigen.setText("$1012.25");
+        panelFondoBlanco.add(txtSaldoOrigen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 280, -1));
+
+        txtValorTransferencia.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtValorTransferencia.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtValorTransferencia.setText("Valor a transferir:  ");
+        panelFondoBlanco.add(txtValorTransferencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 220, 280, -1));
+
+        txtSeleccioneCuenta.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtSeleccioneCuenta.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtSeleccioneCuenta.setText("Seleccione la cuenta destino:");
+        panelFondoBlanco.add(txtSeleccioneCuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 300, 280, -1));
+
+        txtNumeroDeCuenta.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtNumeroDeCuenta.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtNumeroDeCuenta.setText("N° 12324568798");
+        panelFondoBlanco.add(txtNumeroDeCuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 280, -1));
+
+        txtCuentaOrigen.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtCuentaOrigen.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtCuentaOrigen.setText("Cuenta origen:  ");
+        panelFondoBlanco.add(txtCuentaOrigen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 100, 280, -1));
+
+        txtCantidadTransferencia.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtCantidadTransferencia.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtCantidadTransferencia.setText("$156.35");
+        panelFondoBlanco.add(txtCantidadTransferencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 240, 280, -1));
+
+        txtSubtituloDestino.setFont(new java.awt.Font("Segoe UI Variable", 1, 12)); // NOI18N
+        txtSubtituloDestino.setText("CUENTA DESTINO");
+        panelFondoBlanco.add(txtSubtituloDestino, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, -1, -1));
+
+        txtSubtituloResumen.setFont(new java.awt.Font("Segoe UI Variable", 1, 12)); // NOI18N
+        txtSubtituloResumen.setText("RESUMEN DE LA TRANSFERENCIA");
+        panelFondoBlanco.add(txtSubtituloResumen, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, -1, -1));
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        panelFondoBlanco.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 330, 180, -1));
+
+        txtSaldo.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
+        txtSaldo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtSaldo.setText("Saldo disponible:  ");
+        panelFondoBlanco.add(txtSaldo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 160, 280, -1));
+
+        jSeparator1.setBackground(new java.awt.Color(255, 255, 255));
+        jSeparator1.setForeground(new java.awt.Color(204, 204, 204));
+        panelFondoBlanco.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, 260, 20));
+
+        btnConfirmar.setBackground(new java.awt.Color(15, 34, 64));
+        btnConfirmar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        txtConfirmar.setFont(new java.awt.Font("Segoe UI Variable", 1, 13)); // NOI18N
+        txtConfirmar.setForeground(new java.awt.Color(255, 255, 255));
+        txtConfirmar.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtConfirmar.setText("Confirmar");
+
+        javax.swing.GroupLayout btnConfirmarLayout = new javax.swing.GroupLayout(btnConfirmar);
+        btnConfirmar.setLayout(btnConfirmarLayout);
+        btnConfirmarLayout.setHorizontalGroup(
+            btnConfirmarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(txtConfirmar, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+        );
+        btnConfirmarLayout.setVerticalGroup(
+            btnConfirmarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(txtConfirmar, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
+        );
+
+        panelFondoBlanco.add(btnConfirmar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 380, -1, 30));
+
+        btnCancelar.setBackground(new java.awt.Color(227, 29, 26));
+        btnCancelar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        txtCancelar.setFont(new java.awt.Font("Segoe UI Variable", 1, 13)); // NOI18N
+        txtCancelar.setForeground(new java.awt.Color(255, 255, 255));
+        txtCancelar.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtCancelar.setText("Cancelar");
+
+        javax.swing.GroupLayout btnCancelarLayout = new javax.swing.GroupLayout(btnCancelar);
+        btnCancelar.setLayout(btnCancelarLayout);
+        btnCancelarLayout.setHorizontalGroup(
+            btnCancelarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(txtCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+        );
+        btnCancelarLayout.setVerticalGroup(
+            btnCancelarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(txtCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
+        );
+
+        panelFondoBlanco.add(btnCancelar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 380, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 281, Short.MAX_VALUE)
+            .addComponent(panelFondoBlanco, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addComponent(panelFondoBlanco, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -78,5 +228,24 @@ public class TransferenciaCuentaCuenta extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel btnCancelar;
+    private javax.swing.JPanel btnConfirmar;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JPanel panelAzulFooter;
+    private javax.swing.JPanel panelFondoBlanco;
+    private javax.swing.JPanel panelRojoFooter;
+    private javax.swing.JLabel txtCancelar;
+    private javax.swing.JLabel txtCantidadTransferencia;
+    private javax.swing.JLabel txtConfirmar;
+    private javax.swing.JLabel txtCuentaOrigen;
+    private javax.swing.JLabel txtNumeroDeCuenta;
+    private javax.swing.JLabel txtSaldo;
+    private javax.swing.JLabel txtSaldoOrigen;
+    private javax.swing.JLabel txtSeleccioneCuenta;
+    private javax.swing.JLabel txtSubtituloDestino;
+    private javax.swing.JLabel txtSubtituloResumen;
+    private javax.swing.JLabel txtTituloTransferencia;
+    private javax.swing.JLabel txtValorTransferencia;
     // End of variables declaration//GEN-END:variables
 }
