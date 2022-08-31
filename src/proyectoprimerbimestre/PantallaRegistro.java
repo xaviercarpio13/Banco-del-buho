8f45d509b435dfaa3d8d7fee74a57d899429d3f3
Kevin Martinez <kevinprincipal9@gmail.com>
2022-08-30 21:51:21 -0500

    Acomodado de elementos en la pantalla de registro

--- src/proyectoprimerbimestre/PantallaRegistro.java
+++ src/proyectoprimerbimestre/PantallaRegistro.java
@@ -122,66 +122,66 @@ public class PantallaRegistro extends javax.swing.JFrame {
         jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel7.setText("AAAA");
         jLabel7.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 210, 40, 10));
-        jPanel1.add(txtFldCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 150, 180, -1));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 180, 40, 10));
+        jPanel1.add(txtFldCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 130, 180, -1));
 
         lblFecha.setText("Fecha nacimiento:");
-        jPanel1.add(lblFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 190, -1, 22));
+        jPanel1.add(lblFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, -1, 22));
 
         txtFldAnio.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jPanel1.add(txtFldAnio, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 190, 40, -1));
+        jPanel1.add(txtFldAnio, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 160, 40, -1));
 
         txtFldDia.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jPanel1.add(txtFldDia, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 190, 30, -1));
+        jPanel1.add(txtFldDia, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 160, 30, -1));
 
         txtFldMes.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jPanel1.add(txtFldMes, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 190, 30, -1));
+        jPanel1.add(txtFldMes, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 160, 30, -1));
 
         jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel9.setText("-");
         jLabel9.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 190, 10, 22));
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 160, 10, 22));
 
         lblUSD.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lblUSD.setText("USD");
         lblUSD.setToolTipText("");
-        jPanel1.add(lblUSD, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 280, 30, 22));
+        jPanel1.add(lblUSD, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 30, 22));
 
         lblAcceso.setFont(new java.awt.Font("Arial", 3, 12)); // NOI18N
         lblAcceso.setForeground(new java.awt.Color(153, 153, 153));
         lblAcceso.setText("Datos de acceso");
-        jPanel1.add(lblAcceso, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 320, -1, -1));
+        jPanel1.add(lblAcceso, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 300, -1, -1));
 
         jSeparator2.setForeground(new java.awt.Color(153, 153, 153));
-        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 330, 130, 10));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 310, 130, 10));
 
         lblUsuario.setText("Usuario:");
-        jPanel1.add(lblUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 340, -1, 22));
-        jPanel1.add(txtFldUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 340, 160, -1));
+        jPanel1.add(lblUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 320, -1, 22));
+        jPanel1.add(txtFldUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 320, 160, -1));
 
         lblContra.setText("Contraseña:");
-        jPanel1.add(lblContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 370, -1, 22));
-        jPanel1.add(txtFldContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 370, 160, -1));
+        jPanel1.add(lblContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 350, -1, 22));
+        jPanel1.add(txtFldContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 350, 160, -1));
 
         jSeparator3.setForeground(new java.awt.Color(153, 153, 153));
-        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 240, 120, 10));
+        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 220, 120, 10));
 
         lblCreacionCuenta.setFont(new java.awt.Font("Arial", 3, 12)); // NOI18N
         lblCreacionCuenta.setForeground(new java.awt.Color(153, 153, 153));
         lblCreacionCuenta.setText("Creación de cuenta");
-        jPanel1.add(lblCreacionCuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, -1, -1));
+        jPanel1.add(lblCreacionCuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 210, -1, -1));
 
         lblAbono.setText("Abono inicial: ");
-        jPanel1.add(lblAbono, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 280, -1, 22));
+        jPanel1.add(lblAbono, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, -1, 22));
 
         txtFldAbono.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
-        jPanel1.add(txtFldAbono, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 280, 120, -1));
+        jPanel1.add(txtFldAbono, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 260, 120, -1));
 
         lblTipo.setText("Tipo:");
-        jPanel1.add(lblTipo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 250, -1, 22));
+        jPanel1.add(lblTipo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, -1, 22));
 
         cmbTipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { " Ahorros", " Corriente" }));
-        jPanel1.add(cmbTipo, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 250, 150, -1));
+        jPanel1.add(cmbTipo, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 230, 150, -1));
 
         btnVolver.setBackground(new java.awt.Color(227, 29, 26));
         btnVolver.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
@@ -192,7 +192,7 @@ public class PantallaRegistro extends javax.swing.JFrame {
                 btnVolverActionPerformed(evt);
             }
         });
-        jPanel1.add(btnVolver, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 420, 90, -1));
+        jPanel1.add(btnVolver, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 410, 90, -1));
 
         btnFinalizar.setBackground(new java.awt.Color(15, 34, 64));
         btnFinalizar.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
@@ -203,7 +203,7 @@ public class PantallaRegistro extends javax.swing.JFrame {
                 btnFinalizarActionPerformed(evt);
             }
         });
-        jPanel1.add(btnFinalizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 420, 90, -1));
+        jPanel1.add(btnFinalizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 410, 90, -1));
 
         jSeparator4.setForeground(new java.awt.Color(15, 34, 64));
         jPanel1.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, 240, 10));
@@ -211,7 +211,7 @@ public class PantallaRegistro extends javax.swing.JFrame {
         jLabel16.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel16.setText("-");
         jLabel16.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        jPanel1.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 190, 10, 22));
+        jPanel1.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 160, 10, 22));
 
         BloqueRojoFondo.setBackground(new java.awt.Color(227, 29, 26));
         BloqueRojoFondo.setForeground(new java.awt.Color(255, 255, 255));
@@ -230,19 +230,19 @@ public class PantallaRegistro extends javax.swing.JFrame {
         jPanel1.add(BloqueRojoFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 460, 280, 10));
 
         lblCI.setText("C.I:");
-        jPanel1.add(lblCI, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, -1, 22));
+        jPanel1.add(lblCI, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, -1, 22));
 
         jLabel18.setFont(new java.awt.Font("Arial", 0, 10)); // NOI18N
         jLabel18.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel18.setText("DD");
         jLabel18.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        jPanel1.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 210, 30, 10));
+        jPanel1.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 180, 30, 10));
 
         jLabel19.setFont(new java.awt.Font("Arial", 0, 10)); // NOI18N
         jLabel19.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel19.setText("MM");
         jLabel19.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        jPanel1.add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 210, 30, 10));
+        jPanel1.add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 180, 30, 10));
 
         lblCedula.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
         lblCedula.setForeground(new java.awt.Color(255, 0, 0));
